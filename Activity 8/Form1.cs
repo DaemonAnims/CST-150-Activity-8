7b9076b111372dd2b9facb66a987c8c4693596e9
DaemonAnims <daemonanimations3d@gmail.com>
2022-07-27 16:13:26 -0400

    comments update

--- Activity 8/Form1.cs
+++ Activity 8/Form1.cs
@@ -6,22 +6,27 @@ namespace Activity_8
         {
             InitializeComponent();
         }
-
+        //Method for calculating calories from fat grams
         private int FatCalories(int fatGrams)
         {
+            //multiplies grams of fat by 9 to get correct calories
             return fatGrams * 9;
         }
-
+        //Method for calculating calories from carb grams
         private int CarbCalories(int carbGrams)
         {
+            //multiplies grams of carbs by 4 to get correct calories
             return carbGrams * 4;
         }
 
         private void submitButton_Click(object sender, EventArgs e)
         {
+            //get both grams of fat and carbs
             int fatGrams = (int)fatCounter.Value;
             int carbGrams = (int)carbCounter.Value;
+            //add calories from both FatCalories and CarbCalories methods to get total calories 
             int calories = FatCalories(fatGrams) + CarbCalories(carbGrams);
+            //display calories
             answerLabel.Text = calories.ToString("n0") + "cal";
         }
     }
